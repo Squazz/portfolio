f4b80d5cbac5d40d7bf5919ebcb3d56e26726751
Kasper Plougmann <GitHub@squazz.dk>
2018-01-13 17:43:18 +0100

    Initial Commit

---
+++ Portfolio/Program.cs
@@ -0,0 +1,61 @@
+﻿using BinanceExchange.API.Client;
+using BinanceExchange.API.Market;
+using BinanceExchange.API.Models.Request;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace Portfolio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AsyncCall().Wait();
+        }
+
+        private static async Task AsyncCall()
+        {
+            string ApiKey = "API";
+            string SecretKey = "SECRET";
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("BinanceExchange API - Tester");
+            Console.WriteLine("--------------------------");
+
+            //Initialise the general client client with config
+            var client = new BinanceClient(new ClientConfiguration()
+            {
+                ApiKey = ApiKey,
+                SecretKey = SecretKey,
+                //Logger = logger,
+            });
+
+            Console.WriteLine("Interacting with Binance...");
+
+            // Test the Client
+            await client.TestConnectivity();
+            
+            var accountInformation = await client.GetAccountInformation(receiveWindow: 3500); // Get accountinfo
+
+            var balances = accountInformation.Balances.Where(x => x.Free + x.Locked > 0);
+
+            List<BinanceExchange.API.Models.Response.OrderResponse> test = new List<BinanceExchange.API.Models.Response.OrderResponse>();
+
+            foreach (var balance in balances)
+            {
+                if (balance.Asset == "BTC")
+                    continue;
+
+                var data = await client.GetAllOrders(new AllOrdersRequest() {
+                    Symbol = "BTC" + balance.Asset
+                });
+                test.AddRange(data);
+            }
+
+            var stop = 0;
+        }
+    }
+}
